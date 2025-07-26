b287cdf227f6e4e870cfdf0b0d78fa1a9e7b7b35
Waseem Almazrua <gakalmoot12@gmail.com>
2025-07-26 22:51:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,17 @@ def predict():
         open_ = float(request.form['open'])
         high = float(request.form['high'])
         low = float(request.form['low'])
-        volume = int(request.form['volume'])
+        volume = float(request.form['volume'])
 
         input_array = np.array([[open_, high, low, volume]])
-        prediction = model.predict(input_array)[0]
+        input_scaled = scaler.transform(input_array)  
+
+        prediction = model.predict(input_scaled)[0]  
 
         return render_template('index.html', prediction=round(prediction, 2))
     except Exception as e:
         return f"<h2>Error: {str(e)}</h2>"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
